c0bdb32d0f068a33db646b9f70de61f13e38025e
elyxkik <kikuniely@gmail.com>
2024-07-07 19:04:07 +0100

    maj

--- cisep/settings.py
+++ cisep/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9l9x9xmnsy2#-=wteuchjz^_)u0dz8)k1ej-jrst@ot+4rzugf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
